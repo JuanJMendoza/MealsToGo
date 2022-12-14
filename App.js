8426b43988a6402cff9f1969227d29cf0b9b5143
Juan Mendoza <juan.j.mendoza513@gmail.com>
2022-12-14 13:29:30 -0500

    add more utility tools for components

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "styled-components/native";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme/index";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen.js";
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <>
-        <RestaurantScreen />
+        <ThemeProvider theme={theme}>
+          <RestaurantScreen />
+        </ThemeProvider>
         <ExpoStatusBar style="auto" />
       </>
     </SafeAreaProvider>
