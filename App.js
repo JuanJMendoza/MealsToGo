4a133b3be5cd66944b9c5bbf8528bf85f429147f
Juan Mendoza <juan.j.mendoza513@gmail.com>
2022-12-15 23:25:33 -0500

    added tab navigation and icons

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "styled-components/native";
 
@@ -11,6 +14,37 @@ import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
 import { theme } from "./src/infrastructure/theme/index";
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen.js";
+
+import { View, Text } from "react-native";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings!</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map!</Text>
+  </SafeArea>
+);
+
+const TABBARICON = {
+  Restaurants: "restaurant-outline",
+  Settings: "list-outline",
+  Map: "map-outline",
+};
+
+const CreateScreenOptions = ({ route }) => {
+  let iconName = TABBARICON[route.name];
+  return {
+    tabBarIcon: ({ color, size }) => {
+      return <Ionicons name={iconName} size={size} color={color} />;
+    },
+  };
+};
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -27,12 +61,22 @@ export default function App() {
 
   return (
     <SafeAreaProvider>
-      <>
-        <ThemeProvider theme={theme}>
-          <RestaurantScreen />
-        </ThemeProvider>
-        <ExpoStatusBar style="auto" />
-      </>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              CreateScreenOptions,
+              tabBarActiveTintColor: "tomato",
+              tabBarInactiveTintColor: "gray",
+              headerShown: false,
+            }}
+          >
+            <Tab.Screen name="Restaurants" component={RestaurantScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
     </SafeAreaProvider>
   );
 }
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,24 +1,74 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-`;
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { SafeArea } from "../../../components/utility/safe-area.component";
 
 const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
+  padding-bottom: 0px;
 `;
 
-const RestaurantListContainer = styled(View)`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
+const DATA = [
+  {
+    name: "Some Restaurant",
+    icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos: [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address: "100 some random street",
+    isOpenNow: true,
+    rating: 4,
+    isClosedTemporarily: true,
+  },
+  {
+    name: "Some Restaurant",
+    icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos: [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address: "100 some random street",
+    isOpenNow: true,
+    rating: 4,
+    isClosedTemporarily: true,
+  },
+  {
+    name: "Some Restaurant",
+    icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos: [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address: "100 some random street",
+    isOpenNow: true,
+    rating: 4,
+    isClosedTemporarily: true,
+  },
+  {
+    name: "Some Restaurant",
+    icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos: [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address: "100 some random street",
+    isOpenNow: true,
+    rating: 4,
+    isClosedTemporarily: true,
+  },
+  {
+    name: "Some Restaurant",
+    icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
+    photos: [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address: "100 some random street",
+    isOpenNow: true,
+    rating: 4,
+    isClosedTemporarily: true,
+  },
+];
 
 export const RestaurantScreen = () => {
   return (
@@ -26,9 +76,17 @@ export const RestaurantScreen = () => {
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
+      <FlatList
+        data={DATA}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        // keyExtractor={(item) => item.name}
+        key={Math.random() * 1000}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
