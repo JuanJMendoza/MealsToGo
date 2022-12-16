e6c62eec6eedcbca8029e1cc57eeab98c64b68f4
Juan Mendoza <juan.j.mendoza513@gmail.com>
2022-12-15 23:25:09 -0500

    refactor

---
+++ src/components/utility/safe-area.component.js
@@ -0,0 +1,11 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+import styled from "styled-components/native";
+
+export const SafeArea = styled(SafeAreaView)`
+  ${
+    "" /* flex: 1;
+  background-color: blue;
+  padding-bottom: 0;
+  margin-bottom: 0; */
+  }
+`;
